2ccd6ea84cad9f2de74357c9a7118af605e6059d
aletheiaaaaa <carbon674@gmail.com>
2026-07-21 16:38:31 +0100

    redesign x2

--- OpenBench/templatetags/mytags.py
+++ OpenBench/templatetags/mytags.py
@@ -73,18 +73,18 @@ def shortStatBlock(test):
         llr_line = 'LLR: %0.2f (%0.2f, %0.2f) [%0.2f, %0.2f]' % (
             test.currentllr, test.lowerllr, test.upperllr, test.elolower, test.eloupper)
         lower, elo, upper = OpenBench.stats.Elo(test.results())
-        elo_line = 'Elo: %0.2f +- %0.2f (95%%)' % (elo, max(upper - elo, elo - lower))
+        elo_line = 'Elo: %+0.2f ± %0.2f (95%%)' % (elo, max(upper - elo, elo - lower))
         statlines = [llr_line, tri_line, penta_line, elo_line] if test.use_penta else [llr_line, tri_line]
 
     elif test.test_mode == 'GAMES':
         lower, elo, upper = OpenBench.stats.Elo(test.results())
-        elo_line = 'Elo: %0.2f +- %0.2f (95%%) [N=%d]' % (elo, max(upper - elo, elo - lower), test.max_games)
+        elo_line = 'Elo: %+0.2f ± %0.2f (95%%) [N=%d]' % (elo, max(upper - elo, elo - lower), test.max_games)
         statlines = [elo_line, tri_line, penta_line] if test.use_penta else [elo_line, tri_line]
 
     elif test.test_mode == 'DATAGEN':
         status_line = 'Generated %d/%d Games' % (test.games, test.max_games)
         lower, elo, upper = OpenBench.stats.Elo(test.results())
-        elo_line = 'Elo: %0.2f +- %0.2f (95%%) [N=%d]' % (elo, max(upper - elo, elo - lower), test.max_games)
+        elo_line = 'Elo: %+0.2f ± %0.2f (95%%) [N=%d]' % (elo, max(upper - elo, elo - lower), test.max_games)
         statlines = [status_line, elo_line, penta_line] if test.use_penta else [status_line, elo_line, tri_line]
 
     return '\n'.join(statlines)
@@ -101,7 +101,7 @@ def longStatBlock(test):
     lower, elo, upper = OpenBench.stats.Elo(test.results())
 
     lines = [
-        'Elo   | %0.2f +- %0.2f (95%%)' % (elo, max(upper - elo, elo - lower)),
+        'Elo   | %+0.2f ± %0.2f (95%%)' % (elo, max(upper - elo, elo - lower)),
         '%-5s | %s Threads=%d Hash=%dMB' % (type_text, timecontrol, threads, hashmb),
     ]
 
